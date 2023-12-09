cac710282bf4aeb48b23244ac9af06794b62c422
Andrew-R-Y <rakitinandrei@gmail.com>
2023-12-09 22:11:36 +0300

    feat: finish burger menu on main page

--- coffee-house/pages/main.js
+++ coffee-house/pages/main.js
@@ -1,9 +1,11 @@
 const BURGER_BUTTON = document.querySelector('.header__burger-menu');
 const HEADER_NAV_MENU = document.querySelector('.header__nav-list');
 
+// burger menu start
 function openBurger(event) {
   if (event.target.closest('.header__burger-menu')) {
     HEADER_NAV_MENU.classList.toggle('open-nav');
+    BURGER_BUTTON.classList.toggle('open');
   }
   if (
     !event.target.closest('.header__burger-menu') &&
@@ -11,12 +13,15 @@ function openBurger(event) {
       event.target.closest('.link-burger'))
   ) {
     HEADER_NAV_MENU.classList.remove('open-nav');
+    BURGER_BUTTON.classList.remove('open');
   }
 }
+// burger menu end
 
 function escapeFunction(event) {
   if (event.code === 'Escape') {
     HEADER_NAV_MENU.classList.remove('open-nav');
+    BURGER_BUTTON.classList.remove('open');
   }
 }
 
