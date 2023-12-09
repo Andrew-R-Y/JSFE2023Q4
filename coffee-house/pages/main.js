6fc70305e2934d97f4ba55283dc48f668c0c9890
Andrew-R-Y <rakitinandrei@gmail.com>
2023-12-09 21:42:16 +0300

    feat: add open and close burger-menu

--- coffee-house/pages/main.js
+++ coffee-house/pages/main.js
@@ -1,46 +1,24 @@
-console.log(`Score: 90/ 90
+const BURGER_BUTTON = document.querySelector('.header__burger-menu');
+const HEADER_NAV_MENU = document.querySelector('.header__nav-list');
 
-1. The layout of the pages aligns the design at a screen width of 1440px: (14/14):
--[x] <header> block on each page: +2
--[x] Enjoy block on home page: +2
--[x] Favourites Coffee block on home page: +2
--[x] About block on home page: +2
--[x] Mobile App block on home page: +2
--[x] Menu block on menu page: +2
--[x] <footer> block on each page: +2
+function openBurger(event) {
+  if (event.target.closest('.header__burger-menu')) {
+    HEADER_NAV_MENU.classList.toggle('open-nav');
+  }
+  if (
+    !event.target.closest('.header__burger-menu') &&
+    (event.target.closest('.header__nav-link') ||
+      event.target.closest('.link-burger'))
+  ) {
+    HEADER_NAV_MENU.classList.remove('open-nav');
+  }
+}
 
-2. The layout of the pages aligns the design at a screen width of 768px: (14/14)
--[x] <header> block on each page: +2
--[x] Enjoy block on home page: +2
--[x] Favourites Coffee block on home page: +2
--[x] About block on home page: +2
--[x] Mobile App block on home page: +2
--[x] Menu block on menu page: +2
--[x] <footer> block on each page: +2
+function escapeFunction(event) {
+  if (event.code === 'Escape') {
+    HEADER_NAV_MENU.classList.remove('open-nav');
+  }
+}
 
-3. The layout of the pages aligns the design at a screen width of 380px: (14/14)
--[x] <header> block on each page: +2
--[x] Enjoy block on home page: +2
--[x] Favourites Coffee block on home page: +2
--[x] About block on home page: +2
--[x] Mobile App block on home page: +2
--[x] Menu block on menu page: +2
--[x] <footer> block on each page: +2
-
-4. There is no horizontal scrollbar at all screen width up to 380px inclusive. All page content remains as per the design: it is not cropped, removed, or shifted to the side: (20/20)
--[x] home page: no horizontal scroll bar between 1440px and 768px widths: +5
--[x] home page: no horizontal scroll bar between 768px and 380px widths: +5
--[x] menu page: no horizontal scroll bar between 1440px and 768px widths: +5
--[x] menu page: no horizontal scroll bar between 768px and 380px widths: +5
-
-5. During smooth resizing of the browser window from 1440px to 380px, the layout occupies the full width of the window (including specified margins), elements adjust their sizes and positions appropriately without full scaling, no elements overlap, and images maintain their correct aspect ratios: (8/8)
--[x] On home page: +4
--[x] On menu page: +4
-
-6. At screen widths of 768px and below, the menu and navigation buttons in the header are concealed on both pages, and a burger menu icon is displayed: (4/4)
-(Note: Activation of the burger menu icon is not evaluated at this stage.)
-
-7. Hover effects are active on desktop devices (as per the Desktop device type in DevTools) 
-and are disabled for mobile devices (as per the Mobile device type in DevTools) on both pages: (4/4)
-
-8. The layout for both pages is validated and error-free according to the W3C Validator (https://validator.w3.org/): (12/12)`);
+document.addEventListener('click', openBurger);
+document.addEventListener('keyup', escapeFunction);
