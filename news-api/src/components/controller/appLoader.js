762a10f6577969b94a5eebc6b968820e14bef21d
Andrew-R-Y <rakitinandrei@gmail.com>
2024-02-24 12:23:34 +0300

    feat: add API key

--- news-api/src/components/controller/appLoader.js
+++ news-api/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super(process.env.API_URL, {
-            apiKey: process.env.API_KEY,
+            apiKey: '291aaf093f8f4f5a9699663a8414cc4e',
         });
     }
 }
