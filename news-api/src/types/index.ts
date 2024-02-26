7e6817b6ea46431baa723c4ee4cb2539effdcc67
Andrew-R-Y <rakitinandrei@gmail.com>
2024-02-26 19:02:20 +0300

    feat: migrate news.js to news.ts, add .eslintignore, update index.ts

--- news-api/src/types/index.ts
+++ news-api/src/types/index.ts
@@ -1,12 +1,20 @@
 export interface IResponse {
     status: string;
-    sources: {
-        id: string;
+    sources: IData[];
+}
+
+export interface IData {
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+    publishedAt: string;
+    author: string;
+    source: {
+        id: string | null;
         name: string;
-        description: string;
-        url: string;
-        category: string;
-        language: string;
-        country: string;
-    }[];
+    };
+    title: string;
+    urlToImage?: string;
 }
